8d15f23610be8f09d4557c1879d5166cc66a487c
Daniele Tortora <45793868+floroz@users.noreply.github.com>
2024-12-23 12:25:04 +0100

    chore: add strings

--- strings/main.go
+++ strings/main.go
@@ -2,11 +2,49 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"unicode/utf8"
 )
 
-func main() {
-
+// string should be counted by bytes, using len, or by runes
+func countString() {
+	// count bytes
 	fmt.Println(len(`a 🙃`))
+
+	// count runes
+	// these two are equivalents
 	fmt.Println(utf8.RuneCountInString(`a 🙃`))
+	fmt.Println([]rune(`a 🙃`))
+}
+
+// string package has built in methods to manipulate like repeat
+func getInputAndRepeatString() {
+	bangs := strings.Repeat("!", 3)
+
+	var t string
+	fmt.Scanln(&t)
+
+	fmt.Println(strings.ToUpper(t) + bangs)
+
+}
+
+// iota autogenerates number fo constant
+func numberGeneratorIota() {
+	const (
+		monday = iota + 1
+		tuesday
+		wednesday
+		thursday
+		friday
+		saturday
+		sunday
+	)
+
+	fmt.Println(monday, tuesday, wednesday, thursday, friday, saturday, sunday)
+}
+
+func main() {
+	// countString()
+	// getInputAndRepeatString()
+	// numberGeneratorIota()
 }
