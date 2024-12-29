4d462c1313674ea1b0c3ff2a93b7d2f0add492fa
Your Name <45793869+floroz@users.noreply.github.com>
2024-12-29 19:06:59 +0100

    feat: write to file with the reader

--- empty-file-finder/main.go
+++ empty-file-finder/main.go
@@ -4,39 +4,62 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
 )
 
-func checkEmptyFile(path string) {
+const outputFilename = "output.txt"
+
+func checkEmptyFile(path string, outputFile *os.File) {
+	// read dirs
 	dirs, err := os.ReadDir(path)
 
 	if err != nil {
 		panic(err)
 	}
 
+	// no files or directory - early exit
+	if len(dirs) == 0 {
+		return
+	}
+
 	for _, dir := range dirs {
 		nestedPath := filepath.Join(path, dir.Name())
 
+		// a directory
 		if dir.IsDir() {
-			checkEmptyFile(nestedPath)
+			checkEmptyFile(nestedPath, outputFile)
 			continue
 		}
-
-		file, err := os.ReadFile(nestedPath)
+		// a file
+		fileInfo, err := dir.Info()
 
 		if err != nil {
 			panic(err)
 		}
 
-		if content := strings.TrimSpace(string(file)); len(content) == 0 {
-			fmt.Printf("Empty file: %s\n", nestedPath)
-		} else {
-			fmt.Printf("File with content: %s\n", nestedPath)
+		if fileInfo.Size() == 0 {
+			outputFile.WriteString(fmt.Sprintf("Empty file: %s\n", nestedPath))
 		}
+
 	}
 
 }
 
 func main() {
-	checkEmptyFile("./files")
+	// prepare the output file
+	err := os.WriteFile(outputFilename, []byte(nil), 0777)
+
+	if err != nil {
+		panic("failed to clear the output file.")
+	}
+
+	// keep file open to speed up writes
+	outputFile, err := os.OpenFile(outputFilename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+
+	if err != nil {
+		panic("failed to open the output file.")
+	}
+
+	defer outputFile.Close()
+
+	checkEmptyFile("./files", outputFile)
 }
